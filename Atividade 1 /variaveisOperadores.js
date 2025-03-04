37393045aeb45518ab39663502382cd285f38284
Matheus Sales Carvalho <48875053+MatheusSales2773@users.noreply.github.com>
2025-03-04 17:23:47 -0300

    Update variaveisOperadores.js

--- Atividade 1 /variaveisOperadores.js
+++ Atividade 1 /variaveisOperadores.js
@@ -1,1 +1,15 @@
 
+let num1 = 18;
+let num2 = 2;
+
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+
+
+console.log(`Resultado da adição entre ${num1} e ${num2} é ${soma}`);
+console.log(`Resultado da subtração entre ${num1} e ${num2} é ${subtracao}`);
+console.log(`Resultado da multiplicação entre ${num1} e ${num2} é ${multiplicacao}`);
+console.log(`Resultado da divisão entre ${num1} e ${num2} é ${divisao}`);
