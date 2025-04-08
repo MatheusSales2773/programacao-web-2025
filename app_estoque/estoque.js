96702f9401c6b003966062db9027119a5cf65f90
Matheus Sales Carvalho <matheussales497@gmail.com>
2025-04-08 10:03:30 -0300

    atualizando

--- app_estoque/estoque.js
+++ app_estoque/estoque.js
@@ -1,16 +1,1 @@
-const express = rqeuire ('require');
-const app   = express ();
 
-app.get('/', (req, res)=>{
-    let html = '<h1>app_estoque</h1>';
-    html    += '<h3>Rotas disponiveis:</h3>';
-    html    += '<p>/adicionar/:id/:nome/:qtd</p>';
-    html    += '<p>/remover/:id</P>';
-    html    += '<p>/editar/:id/:qtd</p>';
-    res.send()
-});
-
-const PORT = 8080;
-app.listen(PORT, ()=>{
-    console.log('app rodando na porta' + PORT);
-});
